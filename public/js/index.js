c902c4422b2fe4d016726927c08e79ef8798d71d
ssprasath <suriyaprasath@zoho.com>
2018-05-08 01:30:56 +0530

    eit

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,12 @@
 var socket= io();
     socket.on('connect',function(){
         console.log("connected to server");
-
+/* 
         socket.emit('createMessage',{
             from:'prasath@dubukdubuk.com',
             text:'Hey this is demo'
-        });
-    });
+        });*/
+    }); 
    
 socket.on('disconnect',function(){
         console.log("Disconnected from server");
